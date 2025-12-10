46a1afcf9cfdbecfc9b4c154e05499d7b4052dc0
S1iwa <michalsliwa04@gmail.com>
2025-12-10 22:02:59 +0100

    Lista 4ta

--- lista4/Result.h
+++ lista4/Result.h
@@ -21,10 +21,11 @@ public:
   static Result<T, E> Fail(E* error);
   static Result<T, E> Fail(std::vector<E*> errors);
 
+  Result<T, E>& operator=(const Result<T, E>& other);
+
   bool isOk() const;
   T getValue() const;
   std::vector<E*> getErrors() const;
-
 
   Result<double, Error> divide(double dividend, double divisor);
 
@@ -33,6 +34,36 @@ private:
   std::vector<E*> errors;
 };
 
+template <typename T, typename E>
+Result<T, E>::Result(const T& value) {
+
+}
+
+template <typename T, typename E>
+Result<T, E>::Result(E* error) {
+
+}
+
+template <typename T, typename E>
+Result<T, E>::Result(std::vector<E*> errors) {
+
+}
+
+template <typename T, typename E>
+Result<T, E>::Result(const Result<T, E>& other) {
+  if (other.isOk())
+    value = new T(*other.value);
+  else
+    for (E* error : other.errors)
+      errors.push_back(new E(*error));
+}
+
+template <typename T, typename E>
+Result<T, E>::~Result() {
+  delete value;
+  for (E* error : errors)
+    delete error;
+}
 
 template <typename T, typename E>
 Result<double, Error> Result<T, E>::divide(double dividend, double divisor) {
