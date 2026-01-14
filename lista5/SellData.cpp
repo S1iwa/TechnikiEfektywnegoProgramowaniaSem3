3772570dd3f719ebe26761834e9dca3dac6809f2
S1iwa <michalsliwa04@gmail.com>
2026-01-14 19:22:22 +0100

    Popierdoli mnie ale buja

---
+++ lista5/SellData.cpp
@@ -0,0 +1,5 @@
+//
+// Created by micha on 14/01/2026.
+//
+
+#include "SellData.h"
---
+++ lista5/SellData.h
@@ -0,0 +1,10 @@
+//
+// Created by micha on 14/01/2026.
+//
+
+#ifndef TEPSEM3_SELLDATA_H
+#define TEPSEM3_SELLDATA_H
+
+class SellData {};
+
+#endif  // TEPSEM3_SELLDATA_H
